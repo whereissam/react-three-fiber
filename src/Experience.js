bbaa3c542e19ce56635782afc456059084bd8a51
Sam <63341022+whereissam@users.noreply.github.com>
2022-11-27 18:07:19 +0800

    colliders

--- src/Experience.js
+++ src/Experience.js
@@ -1,49 +1,39 @@
-import { useThree, extend, useFrame } from "@react-three/fiber"
-import { useRef } from "react"
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import CustomObject from './CustomObject.js'
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { Debug, Physics, RigidBody } from '@react-three/rapier'
 
-extend({ OrbitControls : OrbitControls })
+export default function Experience()
+{
+    return <>
 
-export default function Experience () {
+        <Perf position="top-left" />
 
-  const { camera, gl } = useThree();
-  console.log(camera, gl)
+        <OrbitControls makeDefault />
 
-  const cubeRef = useRef();
-  const groupRef = useRef();
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+        <Physics>
 
-  useFrame((state, delta) => {
-    // console.log(state)
-    // const angle = state.clock.elapsedTime
-    // state.camera.position.x = Math.sin(angle) * 8
-    // state.camera.position.z = Math.cos(angle) * 8
-    // state.camera.lookAt(0,0,0)
+            <Debug />
 
-    cubeRef.current.rotation.y += 0.01
-    // groupRef.current.rotation.y += delta
-  })
-  return <>
-    <orbitControls args={[ camera, gl.domElement ]} />
-
-    <directionalLight position={[1,2,3]} intensity={1.5}/>
-    <ambientLight intensity={ 0.5 } />
-
-    <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-      <planeGeometry />
-      <meshStandardMaterial color='greenyellow' />
-    </mesh>
-    <group ref={ groupRef }>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color='orange' />
-      </mesh>
-      <mesh ref={cubeRef} rotation-x={Math.PI * 0.25} position-x={[2]} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color='red' wireframe />
-      </mesh>
-    </group>
-   
-   <CustomObject />
-  </>
+            <RigidBody colliders="ball">
+                <mesh castShadow position={ [ 0, 4, 0 ] }>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="orange" />
+                </mesh>
+            </RigidBody>
+            <RigidBody colliders="trimesh">
+                <mesh castShadow position={ [ 0, 1, 0 ]} rotation={[Math.PI * 0.5, 0,0]}>
+                    <torusGeometry args={[ 1, 0.5, 16, 32]} />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </RigidBody>
+            <RigidBody type='fixed'>
+                <mesh receiveShadow position-y={ - 1.25 }>
+                    <boxGeometry args={ [ 10, 0.5, 10 ] } />
+                    <meshStandardMaterial color="greenyellow" />
+                </mesh>
+            </RigidBody>
+        </Physics>
+    </>
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Canvas } from '@react-three/fiber';
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.js'
-import * as THREE from 'three'
-// console.log(THREE.CineonToneMapping)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
 root.render(
-  
-  <Canvas
-  flat
-  dpr={1}
-  gl={{
-    antialias: false,
-    toneMapping: THREE.ACESFilmicToneMapping,
-    outputEncoding: THREE.LinearEncoding
-  }}
-    orthographic
-    camera={{
-      fov: 45,
-      near: 0.1,
-      zoom: 100,
-      far: 200,
-      position: [3,2,6]
-    }}
-  >
-    <Experience />
-  </Canvas>
-);
+    <Canvas
+        shadows
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ 4, 2, 6 ]
+        } }
+    >
+        <Experience />
+    </Canvas>
+)

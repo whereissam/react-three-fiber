7815694d59aad89783f5be00a24a656b78eeaf1f
Sam <63341022+whereissam@users.noreply.github.com>
2022-12-05 10:07:46 +0800

    Add simple porfolio

--- src/Experience.js
+++ src/Experience.js
@@ -1,49 +1,64 @@
-import { useThree, extend, useFrame } from "@react-three/fiber"
-import { useRef } from "react"
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import CustomObject from './CustomObject.js'
-
-extend({ OrbitControls : OrbitControls })
+import { Text, Html, ContactShadows, Float, Environment, useGLTF, PresentationControls } from '@react-three/drei'
+import { RGBA_ASTC_10x10_Format } from 'three'
 
 export default function Experience () {
+  const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
 
-  const { camera, gl } = useThree();
-  console.log(camera, gl)
-
-  const cubeRef = useRef();
-  const groupRef = useRef();
-
-  useFrame((state, delta) => {
-    // console.log(state)
-    // const angle = state.clock.elapsedTime
-    // state.camera.position.x = Math.sin(angle) * 8
-    // state.camera.position.z = Math.cos(angle) * 8
-    // state.camera.lookAt(0,0,0)
-
-    cubeRef.current.rotation.y += 0.01
-    // groupRef.current.rotation.y += delta
-  })
   return <>
-    <orbitControls args={[ camera, gl.domElement ]} />
 
-    <directionalLight position={[1,2,3]} intensity={1.5}/>
-    <ambientLight intensity={ 0.5 } />
+    <Environment preset='city' />
 
-    <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-      <planeGeometry />
-      <meshStandardMaterial color='greenyellow' />
-    </mesh>
-    <group ref={ groupRef }>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color='orange' />
-      </mesh>
-      <mesh ref={cubeRef} rotation-x={Math.PI * 0.25} position-x={[2]} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color='red' wireframe />
-      </mesh>
-    </group>
-   
-   <CustomObject />
+    <color args={['#695b5b']} attach='background' />
+
+    <PresentationControls
+      global
+      rotation={[0.13, 0.1, 0]}
+      polar={[-0.4, 0.2]}
+      azimuth={[-1, 0.75]}
+      config={{ mass: 2, tension: 400 }}
+      snap={{ mass: 4, tension: 400 }}
+    >
+      <Float rotationIntensity={0.4}>
+        <rectAreaLight
+          width={2.5}
+          height={1.65}
+          intensity={65}
+          color={'#0e023b'}
+          rotation={[0.1, Math.PI, 0]}
+          position={[0, 0.55, -1.15]}
+        />
+        <primitive
+          object={computer.scene}
+          position-y={-1.2}
+        >
+          <Html
+            transform
+            wrapperClass='htmlScreen'
+            distanceFactor={1.17}
+            position={[0, 1.56, -1.4]}
+            rotation-x={-0.256}
+          >
+            <iframe src='https://soft-salad-5689.on.fleek.co/' />
+          </Html>
+        </primitive>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          fontSize={1}
+          position={[2, 0.75, 0.75]}
+          rotation-y={-1.25}
+          // children={'SAM '}
+          maxWidth={2}
+          textAlign='center'
+        > Sam
+        </Text>
+      </Float>
+    </PresentationControls>
+
+    <ContactShadows
+      position={-1.4}
+      opacity={0.4}
+      scale={5}
+      blur={2.4}
+    />
   </>
 }

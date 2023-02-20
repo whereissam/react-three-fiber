f5d7ced32ce37d075709de3406275b02550349bc
Sam <63341022+whereissam@users.noreply.github.com>
2023-02-20 20:02:49 +0800

    Initial game setup

--- src/Experience.js
+++ src/Experience.js
@@ -1,49 +1,28 @@
-import { useThree, extend, useFrame } from "@react-three/fiber"
-import { useRef } from "react"
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import CustomObject from './CustomObject.js'
+import { OrbitControls } from '@react-three/drei'
+import Lights from './Lights.js'
 
-extend({ OrbitControls : OrbitControls })
+export default function Experience()
+{
+    return <>
 
-export default function Experience () {
+        <OrbitControls makeDefault />
 
-  const { camera, gl } = useThree();
-  console.log(camera, gl)
+        <Lights />
 
-  const cubeRef = useRef();
-  const groupRef = useRef();
+        <mesh castShadow position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
 
-  useFrame((state, delta) => {
-    // console.log(state)
-    // const angle = state.clock.elapsedTime
-    // state.camera.position.x = Math.sin(angle) * 8
-    // state.camera.position.z = Math.cos(angle) * 8
-    // state.camera.lookAt(0,0,0)
+        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
 
-    cubeRef.current.rotation.y += 0.01
-    // groupRef.current.rotation.y += delta
-  })
-  return <>
-    <orbitControls args={[ camera, gl.domElement ]} />
+        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
 
-    <directionalLight position={[1,2,3]} intensity={1.5}/>
-    <ambientLight intensity={ 0.5 } />
-
-    <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-      <planeGeometry />
-      <meshStandardMaterial color='greenyellow' />
-    </mesh>
-    <group ref={ groupRef }>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color='orange' />
-      </mesh>
-      <mesh ref={cubeRef} rotation-x={Math.PI * 0.25} position-x={[2]} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color='red' wireframe />
-      </mesh>
-    </group>
-   
-   <CustomObject />
-  </>
+    </>
 }

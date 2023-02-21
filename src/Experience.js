4e7a4cb370774df680f6ba38794d1a95893778c9
Sam <63341022+whereissam@users.noreply.github.com>
2023-02-21 20:31:22 +0800

    Add block animation

--- src/Experience.js
+++ src/Experience.js
@@ -1,28 +1,17 @@
 import { OrbitControls } from '@react-three/drei'
+import { Physics, Debug } from '@react-three/rapier'
 import Lights from './Lights.js'
+import Level from './Level.js'
 
-export default function Experience()
-{
+export default function Experience () {
     return <>
 
         <OrbitControls makeDefault />
 
-        <Lights />
-
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+        <Physics>
+            <Lights />
+            <Level />
+        </Physics>
 
     </>
 }
---
+++ src/Level.js
@@ -0,0 +1,72 @@
+import * as THREE from 'three'
+import { RigidBody } from '@react-three/rapier';
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useState } from 'react';
+
+THREE.ColorManagement.legacyMode = false
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+
+
+function BlockStart ({ position = [0, 0, 0] }) {
+  return <group position={position}>
+    <mesh geometry={boxGeometry} material={floor1Material} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} receiveShadow>
+    </mesh>;
+  </group>
+}
+
+function BlockSpinner ({ position = [0, 0, 0] }) {
+
+  const obstacle = useRef()
+  const [speed] = useState(() => (Math.random() * 0.2) * Math.random() * 0.5 ? -1 : 1)
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    // console.log(obstacle)
+    const rotation = new THREE.Quaternion()
+    rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+    obstacle.current.setNextKinematicRotation(rotation)
+  })
+
+  return <group position={position}>
+    <mesh geometry={boxGeometry} material={floor2Material} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} receiveShadow></mesh>;
+
+    <RigidBody ref={obstacle} type="kinematicPosition" position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+      <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]}></mesh>;
+    </RigidBody>
+  </group>
+}
+
+function BlockLimbo ({ position = [0, 0, 0] }) {
+
+  const obstacle = useRef()
+  const [timeOffset] = useState(() => (Math.random() * Math.PI * 2))
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+    const y = Math.sin(time + timeOffset) + 1.15
+    obstacle.current.setNextKinematicTranslation({ x: position[0], y: position[1] + y, z: position[2] })
+  })
+
+  return <group position={position}>
+    <mesh geometry={boxGeometry} material={floor2Material} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} receiveShadow></mesh>;
+
+    <RigidBody ref={obstacle} type="kinematicPosition" position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+      <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]}></mesh>;
+    </RigidBody>
+  </group>
+}
+
+export default function Level () {
+  return <>
+    <BlockStart position={[0, 0, 12]} />
+    <BlockSpinner position={[0, 0, 8]} />
+    <BlockLimbo position={[0, 0, 4]} />
+  </>
+}
